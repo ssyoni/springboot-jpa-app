b7c2884f2454b733c1627eba72fc3eb12fff03c8
ssyoni <jsy950601@gmail.com>
2022-03-15 20:10:52 +0900

    회원 조회 api

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -8,6 +8,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -77,6 +79,48 @@ public class MemberApiController {
     @AllArgsConstructor
     static class UpdateMemberResponse{
         private Long id;
+        private String name;
+    }
+
+    /** 조회 v1: 응답 값으로 엔티티를 직접 외부에 노출한다.
+     * 문제점
+     *  - 엔티티에 프레젠테이션 계층을 위한 로직이 추가된다.
+     *      - 기본적으로 엔티티의 모든 값이 노출된다.
+     *      - 응답 스펙을 맞추기 위해 로직이 추가된다. (@JsonIgnore, 별도의 뷰 로직 등..)
+     *      - 추가 컬렉션을 직접 반환하면 향후 API 스펙을 변경하기 어렵다. (별도의 RESULT 클래스 생성으로 해결)
+     *
+     * 결론
+     * - API 응답 스펙에 맞추어 별도의 DTO를 반환한다.
+     *
+     * 참고
+     * - 엔티티를 외부에 노출하지 말자!(강조*100)
+     * */
+    @GetMapping("/api/v1/members")
+    public List<Member> membersV1(){
+        return memberService.findeMembers();
+    }
+
+    @GetMapping("/api/v2/members")
+    public Result membersV2(){
+
+        List<Member> findMembers = memberService.findeMembers();
+        //엔티티 DTO 변환
+        List<MemberDto> collect = findMembers.stream()
+                .map(member -> new MemberDto(member.getName()))
+                .collect(Collectors.toList());
+
+        return new Result(collect.size());
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T>{
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
         private String name;
     }
 }
